6ce25c8417492c4d9762a91ba3c2c7c476390609
Andy_Dragon <66428908+neveralone-dragon@users.noreply.github.com>
2022-03-29 22:17:46 +0800

    Update 05.替换空格.java

--- 剑指offer面试/05.替换空格.java
+++ 剑指offer面试/05.替换空格.java
@@ -1,13 +1,29 @@
 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
 
 // 1.算法流程：
-初始化一个 list (Python) / StringBuilder (Java) ，记为 res ；
-遍历列表 s 中的每个字符 c ：
-当 c 为空格时：向 res 后添加字符串 "%20" ；
-当 c 不为空格时：向 res 后添加字符 c ；
-将列表 res 转化为字符串并返回。
+// 初始化一个 list (Python) / StringBuilder (Java) ，记为 res ；
+// 遍历列表 s 中的每个字符 c ：
+// 当 c 为空格时：向 res 后添加字符串 "%20" ；
+// 当 c 不为空格时：向 res 后添加字符 c ；
+// 将列表 res 转化为字符串并返回。
 
-作者：jyd
-链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/solution/mian-shi-ti-05-ti-huan-kong-ge-ji-jian-qing-xi-tu-/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// 作者：jyd
+// 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/solution/mian-shi-ti-05-ti-huan-kong-ge-ji-jian-qing-xi-tu-/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray()){
+            if(c==' '){
+                res.append("%20");
+            }else{
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
+
+
